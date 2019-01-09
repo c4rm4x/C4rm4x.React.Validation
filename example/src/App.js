69e0aab0f77389e50320faa1d5ca730029202839
c4rm4x <carmeloquintanahernandez@gmail.com>
2019-01-09 22:17:39 +0000

    Adding example

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react'
+import {Form, required} from 'c4rm4x-react-validation';
 
-import ExampleComponent from 'c4rm4x-react-validation'
+import InputText from './components/inputText';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      test: ''
+    };
+
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
   render () {
     return (
-      <div>
-        <ExampleComponent text='Modern React component module' />
-      </div>
+      <Form onSubmit={e => console.log(e)}>
+        <InputText 
+          name='test' 
+          type='text' 
+          value={this.state.test}
+          onChange={this.onChange}
+          validators={[required]}/>
+      </Form>
     )
   }
 }
